4654efc244c9fe351564616821c8a0bf1f1ece95
Tkachenko AI <alexfisher13@yandex.ru>
2018-02-20 12:52:19 +0300

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {DateService} from "../services/date.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DateService]
 })
 export class AppComponent {
-  title = 'app';
+  month =[[]];
+
+  constructor(private dateService: DateService) {}
+
+  ngOnInit() {
+    this.month = this.dateService.getMonth();
+  }
+
 }
